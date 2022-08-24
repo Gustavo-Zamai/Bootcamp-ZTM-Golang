2af85edfc7362af990388fdf245255fbd4830933
Gustavo Simão Zamai <gustavosimaozamai@gmail.com>
2022-08-24 16:00:40 -0300

    Add examples and start with exercise about function

---
+++ lectures/functions/functionExercise.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main(){
+	fmt.Println("Hi")
+}
+
+func hello(name string) {
+	fmt.Println("Hello,",name,". Nice to meet you!")
+}
+//LESSON 13
--- lectures/functions/functions.go
+++ lectures/functions/functions.go
@@ -2,5 +2,30 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println()
+	resultDouble := double(5)
+	fmt.Println(resultDouble)
+
+	var resultDivide = divide(float32(resultDouble))
+	fmt.Println(resultDivide)
+
+	anotherVariable := add(double(8))
+	fmt.Println("Nested functions that is equal to =", anotherVariable)
+
+	greet()
+}
+
+func double(x int) int{
+	return x * 2
+}
+
+func divide(y float32) float32{
+	return y / 2
+}
+
+func add(a int) int{
+	return a + 2
+}
+
+func greet (){
+	fmt.Println("Hello Chossen one!")
 }
